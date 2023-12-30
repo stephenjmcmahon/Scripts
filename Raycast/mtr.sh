d44b4b0f8b92cca09ba0b42c5181c8a82f9ba327
Stephen McMahon <29905018+stephenjmcmahon@users.noreply.github.com>
2023-12-30 03:19:10 -0800

    Create mtr.sh

---
+++ Raycast/mtr.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# IMPORTANT: Don't forget to give the script "execute" permission by running 'chmod +x mtr.sh'
+
+# Metadata for configuration of the command in Raycast
+# @raycast.schemaVersion 1
+# @raycast.title Run MTR Command
+# @raycast.mode silent
+# @raycast.icon 🌐
+# @raycast.packageName Network
+# @raycast.argument1 { "type": "text", "placeholder": "Enter IP Address" }
+
+# Documentation:
+# @raycast.description This script opens a Terminal window and runs the mtr command with the IP provided.
+# @raycast.author Stephen McMahon
+# @raycast.authorURL https://github.com/stephenjmcmahon
+# @raycast.dependencies Homebrew (https://brew.sh/), mtr (https://formulae.brew.sh/formula/mtr)
+# @raycast.credit mtr (https://github.com/traviscross/mtr)
+
+# Hidden file path
+hidden_file="$HOME/.raycast_mtr_checked"
+
+# Function to check Homebrew and MTR
+check_dependencies() {
+    # Check if Homebrew is installed
+    if ! command -v brew &> /dev/null; then
+        echo "Homebrew could not be found, please ensure it is installed and in the PATH"
+        return 1
+    fi
+
+    # Check if MTR is installed
+    if ! brew list mtr | grep -q 'sbin/mtr'; then
+        echo "mtr could not be found in the expected location (brew list mtr | grep sbin/mtr), please ensure it is installed via Homebrew"
+        return 1
+    fi
+
+    # Mark as checked
+    touch "$hidden_file"
+}
+
+# IP address from Raycast prompt
+ip_address=$1
+
+# Function to run MTR with the provided IP address
+run_mtr() {
+    osascript \
+    -e "tell application \"Terminal\"" \
+    -e "activate" \
+    -e "do script \"mtr $ip_address\"" \
+    -e "end tell"
+}
+
+# Perform checks if hidden file does not exist or rerun checks if mtr fails
+if [ ! -f "$hidden_file" ]; then
+    check_dependencies || exit 1
+fi
+
+# Try to run MTR, rerun checks if it fails
+if ! run_mtr; then
+    rm -f "$hidden_file"
+    check_dependencies || exit 1
+    run_mtr
+fi
